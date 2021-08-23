9afe803f043459269953e42b6417c88934e5824e
Jack Sawyer <jack.h.sawyer@pm.me>
2021-08-22 21:33:36 -0500

    Commit

---
+++ TEST_JSCode/script.js
@@ -0,0 +1,19 @@
+console.log( 8 % 3 );
+
+console.log( 8 % 3 == 2 );
+
+for(var i=8; i>-2; i-=3) {
+    console.log(i);
+}
+
+var x = 0;
+for(var i=1; i<5; i++) {
+    x += i;
+}
+console.log(x);
+
+var x = "0";
+for(var i=1; i<5; i++) {
+    x += i;
+}
+console.log(x);
