238471c0fd0d3f673af2414036e8f88b88eed296
Jack Sawyer <jack.h.sawyer@pm.me>
2021-08-22 22:36:27 -0500

    Commit

---
+++ LoopChallenges/script.js
@@ -0,0 +1,19 @@
+for (var i = 0; i <= 20; i++) {
+    if (i == 0) {
+        console.log(i + " is even");
+    }
+    else if (i % 2 == 0) {
+        console.log(i + " is even");
+    }
+    else {
+        console.log(i + " is odd");
+    }
+}
+
+for (var num = i = 1; i < 100; i++); {
+    var results = num(function (num) {
+        return num % 3 == 0;
+    });
+    console.log(results);
+}
+
